8cc7ad7d4c2cf1adce9e7618ccf1dd8239cf7528
hehuabin <hehuabin@tech-winning.com>
2024-08-03 11:15:33 +0800

    fix

--- contracts/04Variables.sol
+++ contracts/04Variables.sol
@@ -6,12 +6,15 @@ contract Variables {
     string public text = "Hello";
     uint256 public num = 123;
 
-    function doSomething() public {
+
+    function doSomething() public view returns (address){
         // Local variables are not saved to the blockchain.
+        
         uint256 i = 456;
 
         // Here are some global variables
         uint256 timestamp = block.timestamp; // Current block timestamp
         address sender = msg.sender; // address of the caller
+        return sender;
     }
 }
